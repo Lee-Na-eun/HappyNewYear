3a10f9727d6263fc390ebdd88530f0fa1e2a6d11
Lee Naeun <nanni4287@gmail.com>
2021-12-29 20:33:16 +0900

    [Client] test 풀기 초기 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import './App.css';
+import Quiz from './component/quiz';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Quiz />
     </div>
   );
 }
---
+++ client/src/component/quiz.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Quiz() {
+  return (
+    <div>
+      <h1>1/6</h1>
+      <div>당신은 지금 휴양지에 와있습니다. 어디로 가고 싶으신가요?</div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+    </div>
+  );
+}
+
+export default Quiz;
