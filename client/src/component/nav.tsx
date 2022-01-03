cf45a4c14674178dca57496c1edecfdb52857b96
Lee Naeun <nanni4287@gmail.com>
2022-01-03 17:16:51 +0900

    [Client] Nav styled Component

--- client/src/component/nav.tsx
+++ client/src/component/nav.tsx
@@ -5,7 +5,6 @@ function Nav() {
     <NavWrap>
       <ul>
         <li>이거슨 로고</li>
-        <li>이거슨 로그인 메뉴</li>
       </ul>
     </NavWrap>
   );
--- client/src/style/styleNav.tsx
+++ client/src/style/styleNav.tsx
@@ -1,12 +1,16 @@
 import styled, { keyframes } from 'styled-components';
 
 export const NavWrap = styled.div`
-  background-color: #fff;
   position: fixed;
   width: 100vw;
   height: 70px;
   > ul {
     display: flex;
-    flex-direction: row-reverse;
+    height: 70px;
+    line-height: 70px;
+    > li {
+      flex: 1 1 auto;
+      padding: 0 50px;
+    }
   }
 `;
