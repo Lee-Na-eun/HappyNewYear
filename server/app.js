360d4bd5a8e66360ceb7f41ea1a3047e121e96d7
Lee Naeun <nanni4287@gmail.com>
2022-01-05 16:47:47 +0900

    [Server] ts -> js로 변경

---
+++ server/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { sequelize } = require('./models');
+require('dotenv').config();
+
+const serverPort = process.env.SERVER_PORT || 4000;
+const host = process.env.HOST || 'localhost';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+    credentials: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send('Hello Server!');
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Not Found!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({
+    message: 'Internal Server Error',
+    stacktrace: err.toString(),
+  });
+});
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log();
+    console.log(`👍데이터베이스 연결 성공👍 \n`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.listen(serverPort, host, async () => {
+  console.log(`🍎 Server Listening on ${host}:${serverPort} 🍎`);
+});
+
+// app.listen(serverPort, () => {
+//   console.log(`서버 연결 성공 🍎`);
+// });
