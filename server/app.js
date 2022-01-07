68f360572817add8656cb4ab97791ea255f42f85
Lee Naeun <nanni4287@gmail.com>
2022-01-07 17:59:46 +0900

    [Server] server port test

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const { sequelize } = require('./models');
 require('dotenv').config();
 
-const serverPort = process.env.SERVER_PORT || 4000;
+const serverPort = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
