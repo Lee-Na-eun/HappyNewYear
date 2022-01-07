ed5e82709ba765a6b39548f49ae1cd0fd05f5e88
Lee Naeun <nanni4287@gmail.com>
2022-01-07 16:17:25 +0900

    [Server] server port 변경

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const { sequelize } = require('./models');
 require('dotenv').config();
 
-const serverPort = process.env.SERVER_PORT || 4000;
+const serverPort = 80;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
