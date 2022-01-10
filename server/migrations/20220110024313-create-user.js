86f83c100f7851e9954f075837d8626f8873f4a3
Lee Naeun <nanni4287@gmail.com>
2022-01-10 11:48:19 +0900

    [Server] migrate 다시

--- server/migrations/20220110024313-create-user.js
+++ server/migrations/20220110024313-create-user.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      nickname: {
+      userId: {
         type: Sequelize.STRING
       },
       password: {
