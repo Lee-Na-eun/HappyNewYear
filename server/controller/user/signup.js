e7898921c9c221362a74c783a147af73150d4a5d
Lee Naeun <nanni4287@gmail.com>
2022-01-11 11:58:02 +0900

    [Server] signup 비밀번호 복호/암호화 예시 완성

--- server/controller/user/signup.js
+++ server/controller/user/signup.js
@@ -1,5 +1,27 @@
+const crypto = require('crypto-js');
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
   post: async (req, res) => {
+    const newUserData = req.body;
+    const { userId, password } = newUserData;
+
+    // 비밀번호 복호화 암호화 시키기
+    const secretKey = process.env.SECRET_KEY || 'secretKey';
+
+    const encrypted = crypto.AES.encrypt(
+      JSON.stringify(password),
+      secretKey
+    ).toString();
+
+    console.log('비밀번호 암호화', encrypted);
+
+    const bytes = crypto.AES.decrypt(encrypted, secretKey);
+    const decrypted = JSON.parse(bytes.toString(crypto.enc.Utf8));
+
+    console.log('비밀번호 복호화', decrypted);
+
     res.status(200).json({ message: 'sign up ready' });
   },
 };
