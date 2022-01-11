ace2a21068ea328e077516deb2ccb050e67a004a
Lee Naeun <nanni4287@gmail.com>
2022-01-11 15:25:09 +0900

    [Server] signup 완성

--- server/controller/user/signup.js
+++ server/controller/user/signup.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto-js');
+const { user } = require('../../models');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -7,14 +8,27 @@ module.exports = {
     const newUserData = req.body;
     const { userId, password } = newUserData;
 
-    // 비밀번호 복호화 암호화 시키기
-    const secretKey = process.env.SECRET_KEY || 'secretKey';
+    const findUserId = await user.findOne({ where: { userId } });
 
-    const bytes = crypto.AES.decrypt(password, secretKey);
-    const decrypted = JSON.parse(bytes.toString(crypto.enc.Utf8));
+    if (findUserId) {
+      res.status(400).json({ message: 'same UserId' });
+    } else {
+      res.status(200).json({ message: 'ok' });
 
-    console.log('비밀번호 복호화', decrypted);
+      await user.create({
+        userId,
+        password,
+      });
+    }
 
-    res.status(200).json({ message: 'ok' });
+    // 비밀번호 복호화 시키기
+    // const secretKey = process.env.SECRET_KEY || 'secretKey';
+
+    // const bytes = crypto.AES.decrypt(password, secretKey);
+    // const decrypted = JSON.parse(bytes.toString(crypto.enc.Utf8));
+
+    // console.log('비밀번호 복호화', decrypted);
+
+    // res.status(200).json({ message: 'ok' });
   },
 };
