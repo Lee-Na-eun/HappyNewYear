c554e931951ca9471dc5999c4baeb94473e30280
Lee Naeun <nanni4287@gmail.com>
2022-01-16 17:17:59 +0900

    [Client] store.ts 변경

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './rootReducer';
+import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
 
 const store = configureStore({
   reducer,
