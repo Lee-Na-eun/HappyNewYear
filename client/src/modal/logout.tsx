dd4832d87a2e7ddc34d0f4d227cd0eae7317ca8f
Lee Naeun <nanni4287@gmail.com>
2022-01-18 20:21:55 +0900

    [Client] user login 했을 때 user 저장 redux

--- client/src/modal/logout.tsx
+++ client/src/modal/logout.tsx
@@ -1,5 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import {
+  LogoutModal,
+  ButtonWrap,
+  ModalWrap,
+  ModalBox,
+  ContentWrap,
+} from '../style/styleModal';
+import { logoutModalClose } from '../redux/nav/logout';
+import { resultStatus } from '../redux/quiz/result';
+
 function Logout() {
-  return <div>ddhkdhkd</div>;
+  const statusResult = useSelector(resultStatus);
+  const dispatch = useDispatch();
+
+  const handleCloseLogoutModal = () => {
+    dispatch(logoutModalClose());
+  };
+
+  return (
+    <LogoutModal>
+      <ModalWrap>
+        <ModalBox>
+          <ContentWrap>
+            <div>정말 로그아웃 하실 건가요?</div>
+            <ButtonWrap>
+              <button>로그아웃</button>
+              <button onClick={handleCloseLogoutModal}>취소</button>
+            </ButtonWrap>
+          </ContentWrap>
+        </ModalBox>
+      </ModalWrap>
+    </LogoutModal>
+  );
 }
 
 export default Logout;
--- client/src/style/styleModal.tsx
+++ client/src/style/styleModal.tsx
@@ -1,1 +1,78 @@
 import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+    0% {
+      opacity: 0;
+    }
+    100%{
+      opacity: 1;
+    }
+`;
+
+export const LogoutModal = styled.div`
+  > .fadeIn {
+    animation: ${fadeIn} 0.3s ease;
+  }
+  > .fadeOut {
+    display: none;
+    transition: 0.3s;
+  }
+`;
+
+export const ModalWrap = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  > .fadeIn {
+    background-color: red;
+  }
+`;
+
+export const ModalBox = styled.div`
+  width: 500px;
+  height: 250px;
+  background-color: #c56363;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 600px) {
+    width: 90%;
+  }
+`;
+
+export const ContentWrap = styled.div`
+  > div {
+    height: 100px;
+    color: #fff;
+    font-size: 25px;
+    line-height: 100px;
+    @media (max-width: 600px) {
+      font-size: 18px;
+    }
+  }
+`;
+
+export const ButtonWrap = styled.div`
+  > button {
+    width: 100px;
+    height: 40px;
+    margin-left: 20px;
+    border-radius: 10px;
+    border: none;
+    color: #c56363;
+    cursor: pointer;
+    @media (max-width: 600px) {
+      font-size: 12px;
+    }
+  }
+  > button:nth-child(1) {
+    margin-left: 0;
+  }
+`;
