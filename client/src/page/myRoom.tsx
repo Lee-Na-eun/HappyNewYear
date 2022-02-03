031ccec3f4050b206a1f76d238a3111f73f771b9
Lee Naeun <nanni4287@gmail.com>
2022-02-03 14:40:11 +0900

    [Client] findPlan server error 났을 때 alert창

--- client/src/page/myRoom.tsx
+++ client/src/page/myRoom.tsx
@@ -85,17 +85,24 @@ function MyRoom() {
 
         console.log(result);
       }
-    } catch (err) {
-      // swal({
-      //   title: '재로그인이 필요합니다.',
-      //   text: '다시 로그인 후 이용 부탁드립니다.',
-      //   icon: 'warning',
-      // }).then(() => {
-      //   dispatch(logout());
-      //   dispatch(navClose());
-      //   window.location.replace('/');
-      // });
-      console.log(err);
+    } catch (err: any) {
+      if (err.message === 'Network Error') {
+        swal({
+          title: '네트워크가 불안정 합니다.',
+          text: '잠시 후에 이용 부탁드립니다.',
+          icon: 'error',
+        });
+      } else if (err.response.data.message === 'same userId') {
+        swal({
+          title: '재로그인이 필요합니다.',
+          text: '다시 로그인 후 이용 부탁드립니다.',
+          icon: 'warning',
+        }).then(() => {
+          dispatch(logout());
+          dispatch(navClose());
+          window.location.replace('/');
+        });
+      }
     }
   };
 
